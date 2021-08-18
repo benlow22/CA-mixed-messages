01662fc7ab2317a78508d57977c3478d73cc9c59
benlow22 <ben2.2low@gmail.com>
2021-08-18 15:17:08 -0400

    add months, day, randomMonths and randomDay

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log('Hello user')
+console.log('Hello User!!')
+
+const months = [January, February, March, April, May, June, July, August, September, October, November, December]
+const days = Math.ceil(Math.random()*31)
+const randomMonth = months[Math.ceil(Math.random()*12)]
+const randomDay = 
+
+console.log(`Is today... ${randomMonth} ${randomDay}`)
