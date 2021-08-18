1f1d397667526a27918f896cdba323cd62b20601
benlow22 <ben2.2low@gmail.com>
2021-08-18 15:22:54 -0400

    stringify months, no random day

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 console.log('Hello User!!')
 
-const months = [January, February, March, April, May, June, July, August, September, October, November, December]
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 const days = Math.ceil(Math.random()*31)
 const randomMonth = months[Math.ceil(Math.random()*12)]
-const randomDay = 
-
-console.log(`Is today... ${randomMonth} ${randomDay}`)
+console.log(`Is today... ${randomMonth} ${days}?`)
